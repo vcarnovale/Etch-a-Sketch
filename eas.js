46cada131185d6861edf046f185fb4f357adc450
vcarnovale <122923089+vcarnovale@users.noreply.github.com>
2024-08-06 14:25:04 -0400

    Finished functionality for changing the amount of squares

--- eas.js
+++ eas.js
@@ -5,25 +5,55 @@ sketchPad = document.querySelector(".sketch-container");
 var squareAmt = 4;
 const gridWidth = 700;
 const gridHeight = 700;
+
+// Ability to customize height/width of container
 sketchPad.style.height = gridHeight+"px";
 sketchPad.style.width = gridWidth+"px";
 
-var divWidth = gridWidth / squareAmt;
-var divHeight = gridHeight / squareAmt;
-for (var i = 0; i < squareAmt*squareAmt; i++){
-    const newDiv = document.createElement("div");
-    newDiv.classList.add("squares");
-    newDiv.style.width = divWidth-1+"px";
-    newDiv.style.height = divHeight-1+"px";
-    sketchPad.appendChild(newDiv);
-}
 
+// makeSquares (Int squareAmt) --> Makes the squares for the etch-a-sketch grid
+function makeSquares (size){
+    // Sets the amount of pixels dedicated to each square to grow depending on the size of the grid nxn
+    var divWidth = gridWidth / size;
+    var divHeight = gridHeight / size;
 
-const squares = document.querySelectorAll("div");
+    // Loops through the size of the grid and creates the squares for the etch-a-sketcdh
+    for (var i = 0; i < size*size; i++){
+        // Create div
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("squares");
+        
+        // Set height and width to nxn pixel size that fits the default height and width perfectly, -1 to adjust for border
+        newDiv.style.width = divWidth-1+"px";
+        newDiv.style.height = divHeight-1+"px";
+        sketchPad.appendChild(newDiv);
+    }
 
-squares.forEach(square => {
+    // Functionality for button hovering
+    const squares = document.querySelectorAll("div");
+    squares.forEach(square => {
     square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "orange";
+        square.style.backgroundColor = "#FF7F0D";
     })
     
 });
+}
+
+
+// Resets the board
+function resetBoard (size) {
+    for (var i = 0; i < size*size; i++){
+        sketchPad.removeChild(sketchPad.firstChild);
+    }
+}
+
+const setSquares = document.querySelector("#set");
+setSquares.addEventListener("click", () => {
+    var newSize = prompt("Enter how many tiles you would like on each side", 4);
+    resetBoard(squareAmt);
+    squareAmt = newSize;
+    makeSquares(newSize);
+})
+
+
+makeSquares(squareAmt);
